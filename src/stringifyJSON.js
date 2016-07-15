2cffa0fb9805141ad49ce3590cc98ebcbd4d8ed5
Mike Bordelon <374mike@gmail.com>
2016-07-15 00:02:56 -0700

    Re-did this one differently/for fun

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,35 +1,100 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
-
 var stringifyJSON = function(obj){
-
+  
   if(Array.isArray(obj)){
     var result = [];
     for(var i = 0; i < obj.length; i++){
-      result.push(stringifyJSON(obj[i]));
+       result.push(stringifyJSON(obj[i]));
     }
-    return '[' + result.join(',')  + ']';
-  }  
+    return '[' + result + ']';
+  }
 
-  if( typeof obj === 'object' && obj !== null){
+  if( typeof obj === 'object' && obj !== null ){
     var result = [];
     for(var k in obj){
-      if(obj[k] === undefined || typeof obj[k] === 'function'){
-        continue;
-      }
-      result.push( stringifyJSON(k) + ':' + stringifyJSON(obj[k]) );
-    }
-    // type coerces the [] into {}
-    return '{' + result.join(',') + '}';
+      if( typeof obj[k] !== 'function' && obj[k] !== undefined ){
+        result.push(stringifyJSON(k) + ':' + stringifyJSON(obj[k]));
+      }  
+    };
+    return '{' + result + '}';
   }
-  // strings
+
   if(typeof obj === 'string'){
-    return '"' + obj + '"';  
+    return '"' + obj + '"';
   }
-  // non-string primitives
-  return '' + obj;
+  if(typeof obj === 'number'){
+    return '' + obj;
+  }
+  if(obj === null){
+    return 'null';
+  }
+  if(typeof obj === 'boolean'){
+    return '' + obj;
+  }
   
-}
+};
+
+
+// // this is what you would do if you liked things to be easy:
+// // var stringifyJSON = JSON.stringify;
+
+// // but you don't so you're going to write it from scratch:
+
+
+// var stringifyJSON = function(obj){
+
+//   if(Array.isArray(obj)){
+//     var result = [];
+//     for(var i = 0; i < obj.length; i++){
+//       result.push(stringifyJSON(obj[i]));
+//     }
+//     return '[' + result.join(',')  + ']';
+//   }  
+
+//   if( typeof obj === 'object' && obj !== null){
+//     var result = [];
+//     for(var k in obj){
+//       if(obj[k] === undefined || typeof obj[k] === 'function'){
+//         continue;
+//       }
+//       result.push( stringifyJSON(k) + ':' + stringifyJSON(obj[k]) );
+//     }
+//     // type coerces the [] into {}
+//     return '{' + result.join(',') + '}';
+//   }
+//   // strings
+//   if(typeof obj === 'string'){
+//     return '"' + obj + '"';  
+//   }
+//   // non-string primitives
+//   return '' + obj;
+  
+// }
+// strings
+// non-bool primitives
+// booleans
+// objects
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
